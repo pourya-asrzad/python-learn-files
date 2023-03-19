805f1106724d8e06030ad0c181de29139ac5d89c
puria-asr <poriaasrzad@gmail.com>
2023-03-19 13:36:00 +0330

    string section added

---
+++ Primitive-types/string.py
@@ -0,0 +1,37 @@
+sudents_count =1000  
+rating =5.8
+is_published =False
+course_name ="java under the hood , I'm kiding"
+# print(len(course_name))
+# print(course_name[-1])#last of 
+# print(course_name[0:4])#4 from begining
+# print(course_name[0:])#all of
+#print(course_name[:3])#3 first 
+#print(course_name[:])# all of
+
+###########################
+
+# \"
+# \'
+#\\
+# \n
+course ="  python  programing " 
+# print(course)
+
+
+#########################
+first="Mosh"
+last="Hamedani"
+full=first+" "+last
+full_trick=F"{first} {last}"#"f" or "F"
+# print(full)
+
+######################
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())#trim
+print(course.find("m"))#if it's not found we got -1
+print(course.replace("m","n"))
+print("prof" in course)
+print("proh" not in course)
