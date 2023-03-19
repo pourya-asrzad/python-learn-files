f2f83fb42eb36e59cddf4e61589f065e45141cb9
puria-asr <poriaasrzad@gmail.com>
2023-03-19 13:53:10 +0330

    numbers and type-conversion added

---
+++ Primitive-types/number.py
@@ -0,0 +1,22 @@
+import math
+# x=1
+# x=1.1
+# x=1+2j# a+ bi
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+print(10**3)
+
+x=10
+x=x+3
+x+=3
+
+
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
---
+++ Primitive-types/type-convertion.py
@@ -0,0 +1,16 @@
+x= input("X: ")
+# print(type(x))
+y=int(x)+1
+
+print(f"X:{x},y:{y}")
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+#####
+#Falsy
+#''
+#0
+#None
