6802203f9cd1cb94ec291b9a6ad3d7619e32c27a
puria-asr <poriaasrzad@gmail.com>
2023-04-27 23:47:52 +0430

    modules added

--- classes/22-data-classes.py
+++ classes/22-data-classes.py
@@ -3,4 +3,5 @@ from collections import namedtuple
 Point =namedtuple("Point",["x","y"])
 p1 =Point(x=1,y=2)
 p2 =Point(x=1,y=2)
+print(Point)
 print(p1==p2)
---
+++ modules/creating-modules/app.py
@@ -0,0 +1,6 @@
+from sales import calc_shipping ,calc_tax
+import sales 
+
+
+sales.calc_tax()
+calc_shipping()
